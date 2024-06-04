e7b2412f7c61e585ff386521d4b32fb535a8b1ac
AkramHossainOpu <akramhossainopu2002@gmail.com>
2024-06-05 01:05:07 +0600

    Create Customer.cpp

---
+++ Customer.cpp
@@ -0,0 +1,25 @@
+#include <windows.h>
+#include <conio.h>
+#include "Customer.h"
+
+CustomerBST::CustomerBST() {
+    root = nullptr;
+}
+
+void CustomerBST::addCustomer(Customer*& node, string name, string address, string phoneNumber) {
+    if (node == nullptr) {
+        node = new Customer{name, address, phoneNumber, nullptr, nullptr};
+    } else if (name < node->name) {
+        addCustomer(node->left, name, address, phoneNumber);
+    } else {
+        addCustomer(node->right, name, address, phoneNumber);
+    }
+}
+
+void CustomerBST::addCustomer(string name, string address, string phoneNumber) {
+    addCustomer(root, name, address, phoneNumber);
+    system("cls");
+    cout << "\n\n\n\n\n\n\n\n\t\t\t\t\tCustomer added successfully!" << endl;
+    Sleep(1000);
+    system("cls");
+}
